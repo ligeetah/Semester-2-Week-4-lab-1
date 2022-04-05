998c8f9853874db9144584554d7db34b058536ae
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2022-04-05 09:53:46 +0500

    Add project files.

---
+++ Tasks/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tasks.kl;
+
+namespace Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu = new Student("Ahmad", 123, 3.35F, 1098, 1072, "TYur", true, false, 330f);
+            float merit=stu.calculate_merit();
+            Console.WriteLine(merit);
+            Console.WriteLine(stu.is_elegible(merit));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tasks/kl/test.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks.kl
+{
+    internal class Student
+    {
+        String Name;
+        int rollNumber;
+        float cGPA;
+        int matricMarks;
+        int fscMarks;
+        string homeTown;
+        bool isHostelite;
+        bool isTakingScholarship;
+        float ecat;
+        public Student() { }
+        public Student(string names,int roll,float gpa,int matric_marks,int fsc_marks,string add,bool hostel,bool scholar,float ect)
+        {
+            Name = names;
+            rollNumber = roll;
+            cGPA = gpa;
+            matricMarks = matric_marks;
+            fscMarks = fsc_marks;
+            homeTown = add;
+            isHostelite = hostel;
+            isTakingScholarship = scholar;
+            ecat = ect;
+        }
+        public float calculate_merit()
+        {
+            float merit = (fscMarks / 1100F * 60F + ecat / 400F * 40);
+            return merit;
+        }
+        public bool is_elegible(float merit)
+        {
+            if(merit > 80 && isHostelite == true && isTakingScholarship == false)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
